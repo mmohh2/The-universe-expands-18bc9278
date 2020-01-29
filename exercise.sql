be01069b1cb335a38e3bc6a4d5d1565d2ca2446e
mmohh2 <moh19951@outlook.com>
2020-01-29 21:47:20 +0100

    Add files via upload

---
+++ exercise.sql
@@ -0,0 +1,10 @@
+-- CREATE DATABASE Sterrenstelsel;
+-- USE sterrenstelsel;
+-- CREATE TABLE Planeten (name VARCHAR(20));
+-- INSERT INTO planeten (name) VALUES('Mercurius');
+-- INSERT INTO planeten (name) VALUES('Aarde');
+-- INSERT INTO planeten (name) VALUES('Mars');
+-- INSERT INTO planeten (name) VALUES('Venus');
+-- INSERT INTO planeten (name) VALUES('Zon');
+-- SHOW TABLES;
+-- describe planeten;
